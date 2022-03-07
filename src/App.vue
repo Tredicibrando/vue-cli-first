a33294e9f2f66083b98c3c7fb500e30f4a778d81
Tredicibrando <nicolosigiov@gmail.com>
2022-03-07 17:20:42 +0100

    struttura

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,22 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <my-title />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import myTitle from '@/components/myTitle'
+import MyTitle from './components/myTitle.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    myTitle,
+    MyTitle,
   }
 }
 </script>
